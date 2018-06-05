8f071f4dc04043debf314a3eacccd35d228a3926
mplcrj <mplcrj@gmail.com>
2018-06-05 20:02:26 -0300

    tela categorias02

--- index.php
+++ index.php
@@ -224,6 +224,8 @@ $app->post("/admin/forgot/reset",function (){
 });
 
 $app->get("/admin/categories",function (){
+    
+    User::verifyLogin();
 
     $categories = Category::listAll();
 
@@ -236,10 +238,75 @@ $app->get("/admin/categories",function (){
 });
 
 $app->get("/admin/categories/create",function (){
+    
+    User::verifyLogin();
 
     $page = new PageAdmin();
 
     $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create",function (){
+    
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete",function ($idcategory){
+    
+    User::verifyLogin();
+    
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/admin/categories/:idcategory",function ($idcategory){
+    
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);  
+    
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update",[
+        "category"=>$category->getValues()
+    ]);
+
+});
+
+$app->post("/admin/categories/:idcategory",function ($idcategory){
+    
+    User::verifyLogin();
+    
+    $category = new Category();
+    
+    $category->get((int)$idcategory);
+    
+    $category->setData($_POST);
+    
+    $category->save();
+    
+    header('Location: /admin/categories');
+    exit;
 
 });
 
