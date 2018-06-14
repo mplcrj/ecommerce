174881be8f14fc560c39e9628d08aa7222714145
mplcrj <mplcrj@gmail.com>
2018-06-14 20:31:03 -0300

    tela produtovscategoria01

--- site.php
+++ site.php
@@ -15,4 +15,19 @@ $app->get('/', function() {
 
 });
 
+$app->get("/categories/:idcategory",function ($idcategory){
+
+    $category = new Category();
+    
+    $category->get((int)$idcategory);  
+    
+    $page = new Page();
+
+    $page->setTpl("category",[
+        'category'=>$category->getValues(),
+        'products'=>[]
+    ]);
+
+});
+
 ?>
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -32,7 +32,7 @@ class Product extends Model {
         return $list;
         
     }
-
+    
     public function save(){
 
     	$sql = new Sql();
